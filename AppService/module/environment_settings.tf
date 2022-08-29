75462e76570dbbf57c41042298ac1f07d38040ad
davidomahony <daverock914@gmail.com>
2022-08-29 09:56:03 +0100

    Create environment_settings.tf

---
+++ AppService/module/environment_settings.tf
@@ -0,0 +1,23 @@
+locals{
+  environment_settings = {
+    "dev" = {
+      "env" : "dev",
+      "dummy" : "value"
+    }
+    "prod" = {
+      "env" : "prod",
+      "dummy" : "value"
+    }
+  }
+}
+
+resource "kubernetes_config_map" "enviornment_settings" {
+  depends_on = [
+    kubernetes_namespace.namespace  
+  ]
+  metadata {
+    name = "environment_settings"
+    namespace = local.namespace
+  }
+  data = local.environment_settings[var.environment]
+}
